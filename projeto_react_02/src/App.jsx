9be93abc7cc654c1a172470e9e6b6e70fefa4d2c
Izaù santos <nayahto.147@gmail.com>
2023-02-17 23:33:03 -0300

    feat added (register)

--- projeto_react_02/src/App.jsx
+++ projeto_react_02/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Login from "./components/login/login";
+import Register from "./components/login/register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
--- projeto_react_02/src/components/login/register.jsx
+++ projeto_react_02/src/components/login/register.jsx
@@ -1,12 +1,23 @@
+import "./register.css";
+
 function Register() {
   return (
     <>
-      <div id="registerPerson">
-        <input type="text" placeholder="insira seu cpf" />
-        <input type="text" placeholder="insira sua senha" />
-        <input type="text" placeholder="insira nome de usuario" />
-        <input type="text" placeholder="insira seu email" />
-        <input type="text" placeholder="insira o tipo de usuario" />
+      <div id="generalPerson">
+        <h1>insira seus dados</h1>
+        <div id="registerPerson">
+          <label htmlFor="">CPF:</label>
+          <input type="text" placeholder="insira seu cpf" />
+          <label htmlFor="">Senha:</label>
+          <input type="text" placeholder="insira sua senha" />
+          <label htmlFor="">Usuario:</label>
+          <input type="text" placeholder="insira nome de usuario" />
+          <label htmlFor="">Email:</label>
+          <input type="text" placeholder="insira seu email" />
+          <label htmlFor="">Usuario:</label>
+          <input type="text" placeholder="insira o tipo de usuario" />
+        </div>
+        <button>cadastrar</button>
       </div>
     </>
   );
